0592dfeec7bf5aad4c30eec1be22490a21b44c18
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-21 11:39:12 +0300

    commit tree f0db13d0e336e665086737f940c971ea50105ef2

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative './vehicle'
+require_relative '.lib/vehicle'
+
+def Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+      end
+end
+
+corrola = Car.new(23, 4)
+puts corrola.wheel_number
