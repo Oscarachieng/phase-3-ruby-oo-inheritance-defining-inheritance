3d2aa8fb36bb0be03d5359b833388477a38d5fc2
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-21 11:42:08 +0300

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+   attr_accessor :wheel_size, :wheel_number
+    def initialize(wheel_size, wheel_number)
+        @wheel_number = wheel_number
+        @wheel_size = wheel_size
+    end
+
+    def go
+        "vrrrroomm!"
+    end
+
+    def filling_up_tank
+        "filling up tank"
+    end
 
 end
